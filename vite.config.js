dd6d7323c28e65115bc689908e7f2ec4ff1f78f3
Jack Duncan <jhd66@georgetown.edu>
2025-08-13 22:30:15 -0700

    Fix GitHub Pages deployment by adding base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/stinger-v3/',
   plugins: [react()],
   build: {
     outDir: 'dist',
